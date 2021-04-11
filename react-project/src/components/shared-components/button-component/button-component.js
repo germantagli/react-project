07158a7b1e96098b156b7102e9ea394e70709f47
german <germantagli@gmail.com>
2021-04-11 10:42:01 +0200

    shared button completed

--- react-project/src/components/shared-components/button-component/button-component.js
+++ react-project/src/components/shared-components/button-component/button-component.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import "./button-component.scss"
+import { Button } from 'element-react/next';
+import 'element-theme-default';
+
+/** Component created to share it with the rest of the application renders a button based on the properties that are passed
+  *@prop {string}   size      ->large/small/mini
+  *@prop {string}   type      ->primary/success/warning/danger/info/text
+  *@prop {Boolean}  disabled  ->false/true
+  *@prop {string}   icon      ->button icon, accepts an icon name of Element icon component
+  *@prop {string}   label     ->label button
+**/
 
 const ButtonComponent = (props, context) => {
+  const { size,type, disabled, icon, label } = props;
   return (
-    <div>
-        <h1>button component</h1>
-    </div>
+    <Button icon={icon} disabled={disabled} size={size} type={type} >{label}</Button>
   );
 };
 
 export default ButtonComponent;
 
-ButtonComponent.propTypes = {};
