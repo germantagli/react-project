c801385fd16acb2e2968e25e53f8553705b5a74b
german <germantagli@gmail.com>
2021-04-11 20:49:54 +0200

    add file to constant management

---
+++ react-project/src/constants.js
@@ -0,0 +1,28 @@
+export const ARRAY_COLUMS_USER_TABLE = [
+    {
+        label: "id",
+        prop: "id",
+        width: 60
+    },
+    {
+      label: "Email",
+      prop: "email",
+      width: 180
+    },
+    {
+      label: "Name",
+      prop: "name"
+    },
+    {
+        label: "Phone",
+        prop: "phone"
+    },
+    {
+        label: "Username",
+        prop: "username"
+    },
+    {
+        label: "Website",
+        prop: "website"
+    }, 
+  ]
