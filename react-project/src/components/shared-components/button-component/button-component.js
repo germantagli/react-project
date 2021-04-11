64ba95a225ea1ea2520ac36dfb6363d8f27a660e
german <germantagli@gmail.com>
2021-04-11 17:40:04 +0200

    fix function click to button component

--- react-project/src/components/shared-components/button-component/button-component.js
+++ react-project/src/components/shared-components/button-component/button-component.js
@@ -5,17 +5,25 @@ import 'element-theme-default';
 
 const ButtonComponent = (props, context) => {
   /** Component created to share it with the rest of the application renders a button based on the properties that are passed
-    *@prop {string}   size      ->large/small/mini
-    *@prop {string}   type      ->primary/success/warning/danger/info/text
-    *@prop {Boolean}  disabled  ->false/true
-    *@prop {string}   icon      ->button icon, accepts an icon name of Element icon component
-    *@prop {string}   label     ->label button
+    *@prop {string}   size          ->large/small/mini
+    *@prop {string}   type          ->primary/success/warning/danger/info/text
+    *@prop {Boolean}  disabled      ->false/true
+    *@prop {string}   icon          ->button icon, accepts an icon name of Element icon component
+    *@prop {string}   label         ->label button
+    *@prop {function} functionClick ->function that is called when the button is clicked
   **/
 
-  const { size,type, disabled, icon, label } = props;
+  const { size,type, disabled, icon, label, functionClick } = props;
+
+  /** handleClick : manage click component 
+    *@return {void}
+  **/
+  const handleClick = () => {
+      functionClick()
+  }
 
   return (
-    <Button icon={icon} disabled={disabled} size={size} type={type} >{label}</Button>
+    <Button onClick={handleClick} icon={icon} disabled={disabled} size={size} type={type} >{label}</Button>
   );
   
 };
