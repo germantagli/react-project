4a9b0617fef6467f36770ee12b64adfe504361c4
german <germantagli@gmail.com>
2021-04-12 03:12:29 +0200

    bug fixed

--- react-project/src/pages/home-page/home-page.js
+++ react-project/src/pages/home-page/home-page.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import "./home-page.scss"
 import 'element-theme-default';
+import { Alert } from 'element-react/next';
+import { Row, Col } from 'react-bootstrap';
+import img404 from '../../error.png';
+import destopk from '../../screen2.png'; 
+import mobile from '../../screen1.png';  
 
 const HomePage = (props, context) => {
-
   return (
-    <h1>Home</h1>
-  );
-  
+    <div>
+      <Row>
+      <Col md={12}>
+        <Alert
+          title="Informazione"
+          type="info"
+          description="In questa primera versione de la aplicazione, potrai fare : Inserimento, Modifica, Cancellazione e Visualizazione della lista de tutti gli User "
+          showIcon={true} />
+      </Col>
+      
+      <Col md={12} className="text-center mt-5">
+          <h2>Desktop</h2>
+          <img src={destopk} class="img-fluid" alt="" width="75%"></img>
+          <h2>Mobile</h2>
+          <img src={mobile}  class="img-fluid" alt="" width="50%"></img>
+      </Col>
+            
+      <Col md={6} className="text-center mt-5">
+       
+      </Col>
+
+      </Row>
+    </div>
+   );    
 };
 
 export default HomePage;
